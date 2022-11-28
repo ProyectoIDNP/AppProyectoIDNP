617c333de2eaca872c13667117e9aeafb43323c1
Marco Quiña <marcoq558@gmail.com>
2022-11-28 15:31:27 -0500

    Se mejoró el diseño del fragment Categorías

--- app/src/main/java/org/dailyplastic/idnp/prueba/adapters/CategoryGridViewAdapter.java
+++ app/src/main/java/org/dailyplastic/idnp/prueba/adapters/CategoryGridViewAdapter.java
@@ -1,5 +1,6 @@
 package org.dailyplastic.idnp.prueba.adapters;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +9,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 
 import org.dailyplastic.idnp.prueba.model.Category;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.dailyplastic.idnp.R;
 
@@ -34,10 +38,58 @@ public class CategoryGridViewAdapter extends ArrayAdapter<Category>{
         Category categoryModel = getItem(position);
         TextView categoryTextView = listItemView.findViewById(R.id.textViewCategory);
         ImageView categoryImageView = listItemView.findViewById(R.id.imageViewCategory);
+        CardView categoryCardView = listItemView.findViewById(R.id.cardViewCategory);
 
         categoryTextView.setText(categoryModel.getName());
-        categoryImageView.setImageResource(R.drawable.ic_launcher_background);
+
+
+        String categoryAcronym = splitName(categoryModel.getName());
+
+        //Colocacion de las imagenes
+        switch (categoryAcronym) {
+            case "PET":
+                categoryImageView.setImageResource(R.drawable.ic_image_pet);
+                categoryCardView.setCardBackgroundColor(0XFF7A16FC);
+                break;
+            case "PEAD":
+                categoryImageView.setImageResource(R.drawable.ic_image_pead);
+                categoryCardView.setCardBackgroundColor(0XFF6E8166);
+                break;
+            case "PVC":
+                categoryImageView.setImageResource(R.drawable.ic_image_pvc);
+                categoryCardView.setCardBackgroundColor(0XFFC2150A);
+                break;
+            case "PEBD":
+                categoryImageView.setImageResource(R.drawable.ic_image_pebd);
+                categoryCardView.setCardBackgroundColor(0XFFFE561D);
+                break;
+            case "PP":
+                categoryImageView.setImageResource(R.drawable.ic_image_pp);
+                categoryCardView.setCardBackgroundColor(0XFF36D78B);
+                break;
+            case "PS":
+                categoryImageView.setImageResource(R.drawable.ic_image_ps);
+                categoryCardView.setCardBackgroundColor(0XFF8EB6AB);
+                break;
+            case "O":
+                categoryImageView.setImageResource(R.drawable.ic_image_o);
+                categoryCardView.setCardBackgroundColor(0XFF33A3EB);
+                break;
+            default:
+                categoryImageView.setImageResource(R.drawable.ic_image_not_available);
+                categoryCardView.setCardBackgroundColor(0XFF000000);
+                break;
+        }
+
         return listItemView;
     }
+
+    public String splitName(String foo) {
+        String bar = foo. split(" ")[0];
+        return bar;
+    }
+
+
+    
 
 }
--- app/src/main/java/org/dailyplastic/idnp/prueba/fragments/PlasticsCategoriesFragment.java
+++ app/src/main/java/org/dailyplastic/idnp/prueba/fragments/PlasticsCategoriesFragment.java
@@ -37,23 +37,9 @@ public class PlasticsCategoriesFragment extends Fragment {
 
         View plasticCategoriesFragment = inflater.inflate(R.layout.fragment_plastics_categories, container, false);
 
-
         //Obtener los datos del api rest
         getAll();
-
         categoriesGridView = plasticCategoriesFragment.findViewById(R.id.gridViewCategories);
-        /*ArrayList<Category> categoriesModelArrayList = new ArrayList<Category>();
-
-        categoriesModelArrayList.add(new Category(1, "Categoria 1", "10/12/2022", "34/35/2009"));
-        categoriesModelArrayList.add(new Category(2, "Categoria 2", "10/12/2022", "34/35/2009"));
-        categoriesModelArrayList.add(new Category(3, "Categoria 3", "10/12/2022", "34/35/2009"));
-        categoriesModelArrayList.add(new Category(4, "Categoria 4", "10/12/2022", "34/35/2009"));
-        categoriesModelArrayList.add(new Category(5, "Categoria 5", "10/12/2022", "34/35/2009"));
-        categoriesModelArrayList.add(new Category(6, "Categoria 6", "10/12/2022", "34/35/2009"));
-
-        CategoryGridViewAdapter adapter = new CategoryGridViewAdapter(getActivity(), categoriesModelArrayList);
-
-        categoriesGridView.setAdapter(adapter);*/
 
         return plasticCategoriesFragment;
     }
