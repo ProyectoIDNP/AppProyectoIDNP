c3d6966e2731a96f42ceb38ef578bd6c8c1c5fff
Marco Quiña <marcoq558@gmail.com>
2022-12-20 22:05:17 -0500

    Se cambio el tamaño del fragment registrar consumo

---
+++ app/src/main/java/org/dailyplastic/idnp/prueba/fragments/PlasticDetailFragment.java
@@ -0,0 +1,28 @@
+package org.dailyplastic.idnp.prueba.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.dailyplastic.idnp.R;
+
+
+public class PlasticDetailFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plastic_detail, container, false);
+    }
+}
