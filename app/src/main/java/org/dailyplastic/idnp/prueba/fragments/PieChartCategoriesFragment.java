88f89f5ed2c6865d631eab8d325fefbac06b6cd6
Marco Quiña <marcoq558@gmail.com>
2022-12-22 23:54:43 -0500

    Se completó el login y registro

--- app/src/main/java/org/dailyplastic/idnp/prueba/fragments/PieChartCategoriesFragment.java
+++ app/src/main/java/org/dailyplastic/idnp/prueba/fragments/PieChartCategoriesFragment.java
@@ -8,11 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import org.dailyplastic.idnp.R;
 import org.dailyplastic.idnp.prueba.views.PieChart;
 
 public class PieChartCategoriesFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
--- app/src/main/java/org/dailyplastic/idnp/prueba/interfaces/UserService.java
+++ app/src/main/java/org/dailyplastic/idnp/prueba/interfaces/UserService.java
@@ -1,13 +1,17 @@
 package org.dailyplastic.idnp.prueba.interfaces;
 
+import org.dailyplastic.idnp.prueba.dto.UserDto;
 import org.dailyplastic.idnp.prueba.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-public interface RegisterService {
+public interface UserService {
 
     @POST("users/")
-    Call<User> create(@Body User user);
+    Call<User> signIn(@Body User user);
+
+    @POST("auth/login/")
+    Call<UserDto> logIn(@Body User user);
 }
